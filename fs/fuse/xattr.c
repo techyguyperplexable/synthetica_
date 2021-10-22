a33797b7b247a40f5fde766bd1dffa6be47e68a6
Miklos Szeredi <mszeredi@redhat.com>
2021-10-22 17:03:01 +0200

    BACKPORT: fuse: move fuse_invalidate_attr() into fuse_update_ctime()
    Logically it belongs there since attributes are invalidated due to the
    updated ctime.  This is a cleanup and should not change behavior.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: fc5b87cf <90097027+rsuntk@users.noreply.github.com>

--- fs/fuse/xattr.c
+++ fs/fuse/xattr.c
@@ -39,10 +39,9 @@ int fuse_setxattr(struct inode *inode, const char *name, const void *value,
 		fm->fc->no_setxattr = 1;
 		err = -EOPNOTSUPP;
 	}
-	if (!err) {
-		fuse_invalidate_attr(inode);
+	if (!err)
 		fuse_update_ctime(inode);
-	}
+
 	return err;
 }
 
@@ -181,10 +180,9 @@ int fuse_removexattr(struct inode *inode, const char *name)
 		fm->fc->no_removexattr = 1;
 		err = -EOPNOTSUPP;
 	}
-	if (!err) {
-		fuse_invalidate_attr(inode);
+	if (!err)
 		fuse_update_ctime(inode);
-	}
+
 	return err;
 }
 
