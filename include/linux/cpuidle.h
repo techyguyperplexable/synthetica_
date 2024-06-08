8b873c9e78b5a175764882dba21fc59a47e224ad
Alexander Winkowski <dereference23@outlook.com>
2024-06-08 17:49:58 +0000

    lpm-levels: Expose `sleep_disabled`
    Change-Id: Ide920ba562d096cd7d29efc3d09bcb71a7ecbdcf
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -296,4 +296,13 @@ static inline int cpuidle_register_governor(struct cpuidle_governor *gov)
 #define CPU_PM_CPU_IDLE_ENTER_RETENTION(low_level_idle_enter, idx)	\
 	__CPU_PM_CPU_IDLE_ENTER(low_level_idle_enter, idx, 1)
 
+#ifdef CONFIG_MSM_PM
+extern bool lpm_sleep_disabled(void);
+#else
+static bool lpm_sleep_disabled(void)
+{
+	return false;
+}
+#endif
+
 #endif /* _LINUX_CPUIDLE_H */
