43d0416614f83dab611eab7824f11613164a0689
techyguyperplexable <hlcj1234567@gmail.com>
2025-11-24 21:01:30 +0000

    Revert "BACKPORT: kallsyms: refactor {,module_}kallsyms_on_each_symbol"
    This reverts commit 0b648365c9644c5c534c889762bd129551fa6564.

--- kernel/module.c
+++ kernel/module.c
@@ -279,6 +279,11 @@ static void mod_update_bounds(struct module *mod)
 struct list_head *kdb_modules = &modules; /* kdb needs the list of modules */
 #endif /* CONFIG_KGDB_KDB */
 
+static void module_assert_mutex(void)
+{
+	lockdep_assert_held(&module_mutex);
+}
+
 static void module_assert_mutex_or_preempt(void)
 {
 #ifdef CONFIG_LOCKDEP
@@ -4235,7 +4240,8 @@ int module_kallsyms_on_each_symbol(int (*fn)(void *, const char *,
 	unsigned int i;
 	int ret;
 
-	mutex_lock(&module_mutex);
+	module_assert_mutex();
+
 	list_for_each_entry(mod, &modules, list) {
 		/* We hold module_mutex: no need for rcu_dereference_sched */
 		struct mod_kallsyms *kallsyms = mod->kallsyms;
@@ -4250,11 +4256,10 @@ int module_kallsyms_on_each_symbol(int (*fn)(void *, const char *,
 			ret = fn(data, symname(kallsyms, i),
 				 mod, kallsyms->symtab[i].st_value);
 			if (ret != 0)
-				break;
+				return ret;
 		}
 	}
-	mutex_unlock(&module_mutex);
-	return ret;
+	return 0;
 }
 #endif /* CONFIG_KALLSYMS */
 
