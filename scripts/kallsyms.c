3bc2777e823eae0e33b4121e9919ac8b16717122
techyguyperplexable <hlcj1234567@gmail.com>
2025-11-24 21:01:30 +0000

    Revert "BACKPORT: scripts/kallsyms: fix memory corruption caused by write over-run"
    This reverts commit 3e76089ec3153340c8121a9037d8ea348a3e4f15.

--- scripts/kallsyms.c
+++ scripts/kallsyms.c
@@ -210,7 +210,7 @@ static struct sym_entry *read_symbol(FILE *in)
 
 	len = strlen(name) + 1;
 
-	sym = malloc(sizeof(*sym) + len + 1);
+	sym = malloc(sizeof(*sym) + len);
 	if (!sym) {
 		fprintf(stderr, "kallsyms failure: "
 			"unable to allocate required amount of memory\n");
@@ -219,7 +219,7 @@ static struct sym_entry *read_symbol(FILE *in)
 	sym->addr = addr;
 	sym->len = len;
 	sym->sym[0] = type;
-	strcpy(sym_name(sym), name);
+	memcpy(sym_name(sym), name, len);
 	sym->percpu_absolute = 0;
 
 	return sym;
