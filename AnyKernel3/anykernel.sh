20d1ee9d44ca8c56529f996dbb49d99669424e34
techyguyperplexable <hlcj1234567@gmail.com>
2025-11-26 16:51:25 +0000

    AnyKernel: Add my name to buildscript
    Change-Id: I3e0c62c8285314618857074467d37573e5c5b4e0
    Signed-off-by: techyguyperplexable <hlcj1234567@gmail.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=not_Kernel by @skye // pa1n
+kernel.string=not-revived by @kernel2
 do.devicecheck=1
 do.modules=0
 do.systemless=1
