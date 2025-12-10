606bd65d6971b4489023397899c5eec5a052c804
techyguyperplexable <hlcj1234567@gmail.com>
2025-12-10 22:51:02 +0000

    block: Remove ADD_RANDOM
    Change-Id: I3a29ab9b11a787dc98621f19496f91ac5dac4f8f
    Signed-off-by: techyguyperplexable <hlcj1234567@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -720,8 +720,7 @@ struct request_queue {
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
 #define QUEUE_FLAG_PCI_P2PDMA  30	/* device supports PCI p2p requests */
 
-#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_ADD_RANDOM))
+#define QUEUE_FLAG_DEFAULT	(1 << QUEUE_FLAG_SAME_COMP)
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
 				 (1 << QUEUE_FLAG_POLL))
