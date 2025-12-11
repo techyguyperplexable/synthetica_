5218769e5af7b5c6e14ccddce693a4f7c7501431
techyguyperplexable <hlcj1234567@gmail.com>
2025-12-11 16:12:56 +0000

    printk: Reduce default console loglevel to 4 (Warning)
        Stop the kernel from waking up the console/UART for low-priority
        informational messages (info/debug). This reduces CPU wakeups and
        I/O overhead, improving battery life.
    
    Change-Id: Id8e5be57fa49297026ff16e50f90e63f7aa6dd9a
    Signed-off-by: techyguyperplexable <hlcj1234567@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -66,10 +66,10 @@
 #endif
 
 int console_printk[4] = {
-	CONSOLE_LOGLEVEL_DEFAULT,	/* console_loglevel */
+	4,	/* console_loglevel */
 	MESSAGE_LOGLEVEL_DEFAULT,	/* default_message_loglevel */
 	CONSOLE_LOGLEVEL_MIN,		/* minimum_console_loglevel */
-	CONSOLE_LOGLEVEL_DEFAULT,	/* default_console_loglevel */
+	4,	/* default_console_loglevel */
 };
 
 atomic_t ignore_console_lock_warning __read_mostly = ATOMIC_INIT(0);
