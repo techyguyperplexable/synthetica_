139856efed2f458e52b25368c8548ee3a2146b4e
techyguyperplexable <objecting@objecting.org>
2025-12-21 14:03:26 +0000

    boom verify_ok
    Change-Id: I7187e938689478e203aff2780e984d17aaabe80e
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- drivers/kernelsu/kernel_compat.h
+++ drivers/kernelsu/kernel_compat.h
@@ -46,7 +46,7 @@ extern struct key *init_session_keyring;
 
 extern int do_close_fd(unsigned int fd);
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+#ifndef VERIFY_READ
 #define ksu_access_ok(addr, size) access_ok(addr, size)
 #else
 #define ksu_access_ok(addr, size) access_ok(VERIFY_READ, addr, size)
