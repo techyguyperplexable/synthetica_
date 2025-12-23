3759142b0b361b75010006628333ffa5d9db6d0d
techyguyperplexable <objecting@objecting.org>
2025-12-23 21:17:43 +0000

    softirq: Increase limits for better interrupt processing
    Change-Id: I91fe86d1f728989ee3b1bdf868e0c40539aee550
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -211,8 +211,8 @@ EXPORT_SYMBOL(__local_bh_enable_ip);
  * we want to handle softirqs as soon as possible, but they
  * should not be able to lock up the box.
  */
-#define MAX_SOFTIRQ_TIME  msecs_to_jiffies(2)
-#define MAX_SOFTIRQ_RESTART 10
+#define MAX_SOFTIRQ_TIME  msecs_to_jiffies(3)
+#define MAX_SOFTIRQ_RESTART 15
 
 #ifdef CONFIG_TRACE_IRQFLAGS
 /*
