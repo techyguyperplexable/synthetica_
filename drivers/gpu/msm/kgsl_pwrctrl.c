91753894fbc0f8ab3f9a9ba442fa8b2ee3cceb96
techyguyperplexable <objecting@objecting.org>
2025-12-23 21:32:21 +0000

    kgsl: Increase default GPU bus percentage for gaming
    Change-Id: Ieb0b010900603e3ceec58a149a2f518221c8f998
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -30,7 +30,7 @@
 
 #define KGSL_MAX_BUSLEVELS	20
 
-#define DEFAULT_BUS_P 25
+#define DEFAULT_BUS_P 40
 
 /* Order deeply matters here because reasons. New entries go on the end */
 static const char * const clocks[] = {
