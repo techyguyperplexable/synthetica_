294e24866ff3fbc58549ea7898409e01d72eeeda
techyguyperplexable <objecting@objecting.org>
2025-12-23 21:18:40 +0000

    block: Increase default max sectors for larger I/O requests
    Change-Id: I87722c620e07afb4ba5feb87f34676c5b992f18d
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1335,7 +1335,7 @@ extern int blk_verify_command(unsigned char *cmd, fmode_t mode);
 enum blk_default_limits {
 	BLK_MAX_SEGMENTS	= 128,
 	BLK_SAFE_MAX_SECTORS	= 255,
-	BLK_DEF_MAX_SECTORS	= 1024,
+	BLK_DEF_MAX_SECTORS	= 2048,
 	BLK_MAX_SEGMENT_SIZE	= 65536,
 	BLK_SEG_BOUNDARY_MASK	= 0xFFFFFFFFUL,
 };
