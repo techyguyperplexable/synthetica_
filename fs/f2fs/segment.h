94d77c9d6f25a14d37bcedfc118cd7749562ab48
techyguyperplexable <objecting@objecting.org>
2025-12-23 21:20:50 +0000

    f2fs: Tune IPU thresholds for better write performance
    Change-Id: I3abc8e7b8fdda6c06e59ea5c1d642f707a3d065b
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -664,8 +664,8 @@ static inline int utilization(struct f2fs_sb_info *sbi)
  *                            FI_OPU_WRITE flag.
  * F2FS_IPU_DISABLE - disable IPU. (=default option in LFS mode)
  */
-#define DEF_MIN_IPU_UTIL	70
-#define DEF_MIN_FSYNC_BLOCKS	20
+#define DEF_MIN_IPU_UTIL	80
+#define DEF_MIN_FSYNC_BLOCKS	15
 #define DEF_MIN_HOT_BLOCKS	16
 
 #define SMALL_VOLUME_SEGMENTS	(16 * 512)	/* 16GB */
