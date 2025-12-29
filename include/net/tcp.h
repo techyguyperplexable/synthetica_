6cfb39221ff9af6ac5199cce60e0ff67cbaf18bf
techyguyperplexable <objecting@objecting.org>
2025-12-29 08:25:32 +0530

    net: more tcp/wifi optimizations
    - TCP keepalive: 2h->30m, probes 9->6, interval 75s->15s
    - Disable TCP timestamps to reduce packet overhead
    - Reduce conntrack timeouts for mobile networks
    
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -158,9 +158,9 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCP_RESOURCE_PROBE_INTERVAL ((unsigned)(HZ/2U)) /* Maximal interval between probes
 					                 * for local resources.
 					                 */
-#define TCP_KEEPALIVE_TIME	(120*60*HZ)	/* two hours */
-#define TCP_KEEPALIVE_PROBES	9		/* Max of 9 keepalive probes	*/
-#define TCP_KEEPALIVE_INTVL	(75*HZ)
+#define TCP_KEEPALIVE_TIME	(30*60*HZ)	/* 30 minutes */
+#define TCP_KEEPALIVE_PROBES	6		/* Max of 6 keepalive probes	*/
+#define TCP_KEEPALIVE_INTVL	(15*HZ)
 
 #define MAX_TCP_KEEPIDLE	32767
 #define MAX_TCP_KEEPINTVL	32767
