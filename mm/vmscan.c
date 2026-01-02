4c32f074f4969c510ad25e9a95209081ccfb5722
techyguyperplexable <objecting@objecting.org>
2026-01-03 00:39:07 +0530

    mm: vmscan: tune swappiness for better memory management on low RAM devices
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -187,12 +187,12 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 100;
 
 /*
  * Direct reclaim swappiness, values range from 0 .. 60. Higher means more swappy.
  */
-int direct_vm_swappiness = 60;
+int direct_vm_swappiness = 40;
 
 /*
  * The total number of pages which are beyond the high watermark within all
