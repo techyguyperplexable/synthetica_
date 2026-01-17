454b740ed50418becb4f39f269c9062342b24ed0
Z3phery <dz.ignacioaguilar@gmail.com>
2026-01-17 04:24:09 -0300

    set zram swappiness to 160
    Signed-off-by: Z3phery <dz.ignacioaguilar@gmail.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3619,13 +3619,13 @@ static int mem_cgroup_swappiness_write(struct cgroup_subsys_state *css,
 {
 	struct mem_cgroup *memcg = mem_cgroup_from_css(css);
 
-	if (val > 100)
+	if (val > 160)
 		return -EINVAL;
 
 	if (css->parent)
 		memcg->swappiness = val;
 	else
-		; // vm_swappiness = val;
+		vm_swappiness = val;
 
 	return 0;
 }
