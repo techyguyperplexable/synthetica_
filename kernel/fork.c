4a550fd0ab057d5886b85e7698cf1e63fd82e5be
Z3phery <dz.ignacioaguilar@gmail.com>
2026-01-17 04:56:01 -0300

    Restore fork.c and internal.h files
    Signed-off-by: Z3phery <dz.ignacioaguilar@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,8 +95,6 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
-#include <linux/devfreq_boost.h>
-#include <linux/cpu_input_boost.h>
 
 #include <linux/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2332,12 +2330,6 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
-
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current)) {
-		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
-		cpu_input_boost_kick_max(50);
-	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
