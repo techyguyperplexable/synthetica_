94fa6bb35253d18c442e97a16733d8b73a1d2ecd
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-01-24 00:34:11 +0800

    block/ssg: fix NULL pointer dereference
    Fixes:
    4706cef306664211aad8823275b6f77e60b9ef33 block/ssg: fix unsafe RCU access in ssg_blkcg_shallow_depth
    
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- block/ssg-cgroup.c
+++ block/ssg-cgroup.c
@@ -122,12 +122,18 @@ unsigned int ssg_blkcg_shallow_depth(struct request_queue *q)
 
 	rcu_read_lock();
 	blkg = blkg_lookup(css_to_blkcg(blkcg_css()), q);
+	if (!blkg)
+		goto out;
+
 	ssg_blkg = BLKG_TO_SSG_BLKG(blkg);
 
-	if (!IS_ERR_OR_NULL(ssg_blkg)) {
-		if (atomic_read(&ssg_blkg->current_rqs) >= ssg_blkg->max_available_rqs)
-			ret = ssg_blkg->shallow_depth;
-	}
+	if (IS_ERR_OR_NULL(ssg_blkg))
+		goto out;
+
+	if (atomic_read(&ssg_blkg->current_rqs) >= ssg_blkg->max_available_rqs)
+		ret = ssg_blkg->shallow_depth;
+
+out:
 	rcu_read_unlock();
 
 	return ret;
