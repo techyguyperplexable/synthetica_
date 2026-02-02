techyguyperplexable/synthetica_

commit dfc8d6a0e0b25009220f7cc9041a34c5f9a65645

Battery life improvements: enable power-efficient workqueues and fix schedutil boosts
Signed-off-by: techyguyperplexable <objecting@objecting.org>

kernel/sched/cpufreq_schedutil.c
@@ -402,6 +402,8 @@ static void sugov_get_util(struct sugov_cpu *sg_cpu, unsigned long boost)
 
 	util = effective_cpu_util(sg_cpu->cpu, util, &min, &max);
 	util = max(util, boost);
+	util = sugov_apply_all_boosts(util);
+	util = sugov_apply_all_boosts(util);
 	sg_cpu->bw_min = min;
 	sg_cpu->util = sugov_effective_cpu_perf(sg_cpu->cpu, util, min, max);
 }

kernel/workqueue.c
@@ -306,7 +306,7 @@ static bool wq_disable_numa;
 module_param_named(disable_numa, wq_disable_numa, bool, 0444);
 
 /* see the comment above the definition of WQ_POWER_EFFICIENT */
-static bool wq_power_efficient = false;
+static bool wq_power_efficient = true;
 module_param_named(power_efficient, wq_power_efficient, bool, 0644);
 
 static bool wq_online;			/* can kworkers be created yet? */

commit f62d88c03a3685a5bfb7f1863644d59756a7a6db

cpufreq: schedutil: fix duplicate boost application causing erratic scaling
Remove duplicate call to sugov_apply_all_boosts() which was applying
frequency boosts twice. This could cause excessive CPU frequency
requests leading to thermal throttling or system instability.

Fixes: dfc8d6a0e0b2 ("Battery life improvements: enable power-efficient workqueues and fix schedutil boosts")
Signed-off-by: techyguyperplexable <objecting@objecting.org>

kernel/sched/cpufreq_schedutil.c
@@ -403,7 +403,6 @@ static void sugov_get_util(struct sugov_cpu *sg_cpu, unsigned long boost)
 	util = effective_cpu_util(sg_cpu->cpu, util, &min, &max);
 	util = max(util, boost);
 	util = sugov_apply_all_boosts(util);
-	util = sugov_apply_all_boosts(util);
 	sg_cpu->bw_min = min;
 	sg_cpu->util = sugov_effective_cpu_perf(sg_cpu->cpu, util, min, max);
 }