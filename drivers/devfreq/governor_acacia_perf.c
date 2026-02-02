c7f1942ec64807e57a9bc579426c3e21169cd61f
techyguyperplexable <objecting@objecting.org>
2026-02-02 20:06:28 +0000

    add acacia_perf and acacia_batt gpu governors

---
+++ drivers/devfreq/governor_acacia_perf.c
@@ -0,0 +1,65 @@
+/*
+ *  drivers/devfreq/governor_acacia_perf.c
+ *
+ *  Copyright (C) 2024 Acacia
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/devfreq.h>
+#include <linux/module.h>
+#include "governor.h"
+
+static int devfreq_acacia_perf_func(struct devfreq *df,
+				    unsigned long *freq)
+{
+	/*
+	 * Sets the frequency at the maximum available frequency.
+	 */
+	if (!df->max_freq)
+		*freq = UINT_MAX;
+	else
+		*freq = df->max_freq;
+	return 0;
+}
+
+static int devfreq_acacia_perf_handler(struct devfreq *devfreq,
+				unsigned int event, void *data)
+{
+	int ret = 0;
+
+	if (event == DEVFREQ_GOV_START) {
+		mutex_lock(&devfreq->lock);
+		ret = update_devfreq(devfreq);
+		mutex_unlock(&devfreq->lock);
+	}
+
+	return ret;
+}
+
+static struct devfreq_governor devfreq_acacia_perf = {
+	.name = "acacia_perf",
+	.get_target_freq = devfreq_acacia_perf_func,
+	.event_handler = devfreq_acacia_perf_handler,
+};
+
+static int __init devfreq_acacia_perf_init(void)
+{
+	return devfreq_add_governor(&devfreq_acacia_perf);
+}
+subsys_initcall(devfreq_acacia_perf_init);
+
+static void __exit devfreq_acacia_perf_exit(void)
+{
+	int ret;
+
+	ret = devfreq_remove_governor(&devfreq_acacia_perf);
+	if (ret)
+		pr_err("%s: failed remove governor %d\n", __func__, ret);
+
+	return;
+}
+module_exit(devfreq_acacia_perf_exit);
+MODULE_LICENSE("GPL");
