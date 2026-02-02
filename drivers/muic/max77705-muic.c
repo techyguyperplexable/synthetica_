8cb0ba33cdd655b6519f5dc157686c5c2cfe48a9
techyguyperplexable <objecting@objecting.org>
2026-02-02 19:37:55 +0000

    muic: max77705 - disable debug prints
    Disable the debug prints in max77705_muic_print_reg_log to reduce dmesg spam.
    
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- drivers/muic/max77705-muic.c
+++ drivers/muic/max77705-muic.c
@@ -2113,20 +2113,20 @@ static void max77705_muic_print_reg_log(struct work_struct *work)
 	max77705_muic_read_reg(i2c, MAX77705_USBC_REG_PD_STATUS0, &status[6]);
 	max77705_muic_read_reg(i2c, MAX77705_USBC_REG_PD_STATUS1, &status[7]);
 
-	pr_info("%s:%s USBC1:0x%02x, USBC2:0x%02x, BC:0x%02x, UICINTM:0x%x, attached_dev:%d\n",
-		MUIC_DEV_NAME, __func__, status[0], status[1], status[2], status[3],
-		muic_data->attached_dev);
-	pr_info("%s:%s CC_STATUS0:0x%x, CC_STATUS1:0x%x, PD_STATUS0:0x%x, PD_STATUS1:0x%x, WDT:%d, POR:%d\n",
-		MUIC_DEV_NAME, __func__, status[4], status[5], status[6], status[7],
-		muic_data->usbc_pdata->watchdog_count, muic_data->usbc_pdata->por_count);
-	pr_info("%s fail to read uiadc(%d), chgtyp(%d), spr(%d), dcdtmo(%d), vbadc(%d), vbusdet(%d), unknown(%d)\n",
-		__func__, muic_data->irq_fail_count[MAX77705_MUIC_IRQ_UIADC], 
-			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_CHGTYP], 
-			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_SPR], 
-			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_DCDTMO], 
-			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_VBADC], 
-			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_VBUSDET],
-			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_UNKNOWN]);
+//	pr_info("%s:%s USBC1:0x%02x, USBC2:0x%02x, BC:0x%02x, UICINTM:0x%x, attached_dev:%d\n",
+//		MUIC_DEV_NAME, __func__, status[0], status[1], status[2], status[3],
+//		muic_data->attached_dev);
+//	pr_info("%s:%s CC_STATUS0:0x%x, CC_STATUS1:0x%x, PD_STATUS0:0x%x, PD_STATUS1:0x%x, WDT:%d, POR:%d\n",
+//		MUIC_DEV_NAME, __func__, status[4], status[5], status[6], status[7],
+//		muic_data->usbc_pdata->watchdog_count, muic_data->usbc_pdata->por_count);
+//	pr_info("%s fail to read uiadc(%d), chgtyp(%d), spr(%d), dcdtmo(%d), vbadc(%d), vbusdet(%d), unknown(%d)\n",
+//		__func__, muic_data->irq_fail_count[MAX77705_MUIC_IRQ_UIADC], 
+//			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_CHGTYP], 
+//			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_SPR], 
+//			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_DCDTMO], 
+//			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_VBADC], 
+//			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_VBUSDET],
+//			muic_data->irq_fail_count[MAX77705_MUIC_IRQ_UNKNOWN]);
 
 	schedule_delayed_work(&(muic_data->debug_work), msecs_to_jiffies(60000));
 }
