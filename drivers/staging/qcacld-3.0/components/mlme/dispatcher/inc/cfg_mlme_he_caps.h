techyguyperplexable/synthetica_

commit cd7fd2a37bcda3a426bdbae16da018868516a3bb

qcacld-3.0: optimize HE (802.11ax) capabilities
- Enable HE LDPC and Tx/Rx STBC (LT80 and GT80).
- Enable HE SU/MU Beamformer and Beamformee.
- Enable HE MU PPDU and maximize UL MU-MIMO support.
These changes ensure full 802.11ax feature utilization for maximum
performance and stability.

Signed-off-by: techyguyperplexable <objecting@objecting.org>

drivers/staging/qcacld-3.0/components/mlme/dispatcher/inc/cfg_mlme_he_caps.h
@@ -250,7 +250,7 @@
 
 #define CFG_HE_LDPC CFG_BOOL( \
 				"he_ldpc", \
-				0, \
+				1, \
 				"He Ldpc")
 
 #define CFG_HE_LTF_PPDU CFG_UINT( \
@@ -279,12 +279,12 @@
 
 #define CFG_HE_TX_STBC_LT80 CFG_BOOL( \
 				"he_tx_stbc_lt80_sta", \
-				0, \
+				1, \
 				"He Tx Stbc Lt80")
 
 #define CFG_HE_RX_STBC_LT80 CFG_BOOL( \
 				"he_rx_stbc_lt80", \
-				0, \
+				1, \
 				"He Rx Stbc Lt80")
 
 #define CFG_HE_DOPPLER CFG_UINT( \
@@ -313,22 +313,22 @@
 
 #define CFG_HE_MU_PPDU CFG_BOOL( \
 				"he_mu_ppdu", \
-				0, \
+				1, \
 				"He Mu Ppdu")
 
 #define CFG_HE_SU_BEAMFORMER CFG_BOOL( \
 				"he_su_beamformer", \
-				0, \
+				1, \
 				"He Su Beamformer")
 
 #define CFG_HE_SU_BEAMFORMEE CFG_BOOL( \
 				"he_su_beamformee", \
-				0, \
+				1, \
 				"He Su Beamformee")
 
 #define CFG_HE_MU_BEAMFORMER CFG_BOOL( \
 				"he_mu_beamformer", \
-				0, \
+				1, \
 				"He Mu Beamformer")
 
 #define CFG_HE_BFEE_STS_LT80 CFG_UINT( \
@@ -431,12 +431,12 @@
 
 #define CFG_HE_RX_STBC_GT80 CFG_BOOL( \
 				"he_rx_stbc_gt80", \
-				0, \
+				1, \
 				"He Rx Stbc Gt80")
 
 #define CFG_HE_TX_STBC_GT80 CFG_BOOL( \
 				"he_Tx_stbc_gt80", \
-				0, \
+				1, \
 				"He Tx Stbc Gt80")
 
 #define CFG_HE_ER_4x_LTF_GI CFG_BOOL( \
@@ -712,7 +712,7 @@
 				"he_ul_mumimo", \
 				0, \
 				3, \
-				0, \
+				3, \
 				CFG_VALUE_OR_DEFAULT, \
 				"He Ul Mumimo")
 

commit 777a1aa49771cd461423b2afb9800c09fc22c606

qcacld-3.0: disable HE optimization features
Revert recent HE (802.11ax) optimizations that caused stability issues.
Restores default disabled state for LDPC, STBC, and Beamforming features.

Fixes: cd7fd2a37bcd ("qcacld-3.0: optimize HE (802.11ax) capabilities")
Signed-off-by: techyguyperplexable <objecting@objecting.org>

drivers/staging/qcacld-3.0/components/mlme/dispatcher/inc/cfg_mlme_he_caps.h
@@ -250,7 +250,7 @@
 
 #define CFG_HE_LDPC CFG_BOOL( \
 				"he_ldpc", \
-				1, \
+				0, \
 				"He Ldpc")
 
 #define CFG_HE_LTF_PPDU CFG_UINT( \
@@ -279,12 +279,12 @@
 
 #define CFG_HE_TX_STBC_LT80 CFG_BOOL( \
 				"he_tx_stbc_lt80_sta", \
-				1, \
+				0, \
 				"He Tx Stbc Lt80")
 
 #define CFG_HE_RX_STBC_LT80 CFG_BOOL( \
 				"he_rx_stbc_lt80", \
-				1, \
+				0, \
 				"He Rx Stbc Lt80")
 
 #define CFG_HE_DOPPLER CFG_UINT( \
@@ -313,22 +313,22 @@
 
 #define CFG_HE_MU_PPDU CFG_BOOL( \
 				"he_mu_ppdu", \
-				1, \
+				0, \
 				"He Mu Ppdu")
 
 #define CFG_HE_SU_BEAMFORMER CFG_BOOL( \
 				"he_su_beamformer", \
-				1, \
+				0, \
 				"He Su Beamformer")
 
 #define CFG_HE_SU_BEAMFORMEE CFG_BOOL( \
 				"he_su_beamformee", \
-				1, \
+				0, \
 				"He Su Beamformee")
 
 #define CFG_HE_MU_BEAMFORMER CFG_BOOL( \
 				"he_mu_beamformer", \
-				1, \
+				0, \
 				"He Mu Beamformer")
 
 #define CFG_HE_BFEE_STS_LT80 CFG_UINT( \
@@ -431,12 +431,12 @@
 
 #define CFG_HE_RX_STBC_GT80 CFG_BOOL( \
 				"he_rx_stbc_gt80", \
-				1, \
+				0, \
 				"He Rx Stbc Gt80")
 
 #define CFG_HE_TX_STBC_GT80 CFG_BOOL( \
 				"he_Tx_stbc_gt80", \
-				1, \
+				0, \
 				"He Tx Stbc Gt80")
 
 #define CFG_HE_ER_4x_LTF_GI CFG_BOOL( \
@@ -712,7 +712,7 @@
 				"he_ul_mumimo", \
 				0, \
 				3, \
-				3, \
+				0, \
 				CFG_VALUE_OR_DEFAULT, \
 				"He Ul Mumimo")
 