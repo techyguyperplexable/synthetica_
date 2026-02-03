5775afd9025986a51c3fab2bde840cbc055dcf6c
skye-pa1n <sonicgames772@gmail.com>
2026-02-03 17:29:54 -0300

    sched/core: Block harmful CPU affinity changes by Unity & UE4 based games
    Inspired by an original commit kerneltoast/android_kernel_google_gs201@7ecd641
    from Sultan Alsawaf at Kerneltoast, this patch extends the check to ignore setaffinity
    requests from multiple problematic processes & block's problematic games like PUBG.
    By identifying these processes and ignoring their affinity requests, we ensure they
    cannot degrade their own performance by misconfiguring CPU affinity.
    
    Signed-off-by: skye-pa1n <sonicgames772@gmail.com>
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1502,6 +1502,9 @@ struct task_struct {
 	/* Used by LSM modules for access restriction: */
 	void				*security;
 #endif
+
+	bool is_sar_game;
+
 	/* task is frozen/stopped (used by the cgroup freezer) */
 	ANDROID_KABI_USE(1, unsigned frozen:1);
 
