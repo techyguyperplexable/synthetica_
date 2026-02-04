56759bafdfdedf0e17f3eb5f72d2a4baf4e2de65
Z3phery <dz.ignacioaguilar@gmail.com>
2026-02-05 01:45:40 +0600

    Revert "block: get rid of q->softirq_done_fn()"
    This reverts commit 0e4c81b94de87cefe0cc368ba5c41e8a1981a50e.

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -618,7 +618,7 @@ static void __blk_mq_complete_request(struct request *rq)
 		if (!smp_call_function_single_async(ctx->cpu, &rq->csd))
 			goto out;
 	}
-	q->mq_ops->complete(rq);
+	rq->q->softirq_done_fn(rq);
 
 out:
 	put_cpu();
